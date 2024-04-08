7e53b6773eedd7a5840139efefdd5d64844bad78
Jahir-Amir <Amir.Jahir@city.ac.uk>
2024-04-08 13:40:27 +0100

    Summary
    Testing some capabilities of JavaFx

---
+++ FrontOfHouse/src/main/java/org/main/HelloFX.java
@@ -0,0 +1,14 @@
+package org.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class HelloFX extends Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public void start (Stage primaryStage) throws Exception{
+
+    }
+}
